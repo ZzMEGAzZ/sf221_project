606d639428614a34617bf934916d797834d763da
ZzMEGAzZ <apisitsangkrajang@gmail.com>
2023-01-26 17:11:48 +0700

    API FireBase & ios google service

---
+++ lib/shared/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appId = "1:167319168231:web:05f7364f1b165dd7772e42.";
+  static String apiKey = "AIzaSyDm9S6w1mE-MCc0RIyxcwbFM8FMKz3hoVg";
+  static String messagingSenderId = "634480644405";
+  static String projectId = "sf221-project";
+  final primaryColor = const Color(0xFFee7b64);
+}
